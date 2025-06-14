5702af58746a393d6559f515a32a466432b537d6
dan.toropov <dan.toropov@gmail.com>
2025-06-14 23:18:14 +0300

    feat: test add payments

--- tg_app/bot/handlers/user.py
+++ tg_app/bot/handlers/user.py
@@ -1,6 +1,8 @@
-from aiogram import Router, F
+import os
+
+from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, LabeledPrice, PreCheckoutQuery
 
 from tg_app.bot.databases.sql_requests import set_user
 from tg_app.bot.keyboards import userkb
@@ -11,6 +13,25 @@ user = Router ()
 async def start(message: Message):
     await set_user(message.from_user.id)
     await message.answer('Links bellow', reply_markup=userkb.main)
+
+@user.callback_query(F.data=='donate')
+async def top_up(callback_query: CallbackQuery):
+        await callback_query.bot.send_invoice(
+        callback_query.from_user.id,
+        title="Подписка на бота",
+        description="Активация подписки на бота на 1 месяц",
+        provider_token=(os.getenv('TG_PAYMASTER')),
+        currency="rub",
+        photo_url="https://cs15.pikabu.ru/post_img/big/2024/08/24/10/1724515420160483017.png",
+        photo_width=416,
+        photo_height=234,
+        photo_size=416,
+        is_flexible=False,
+        prices=[
+            LabeledPrice(label='Подписка на бота (1 месяц)', amount=30000)
+        ],
+        start_parameter="",
+        payload="test-invoice-payload")
 
 @user.callback_query(F.data=='menu')
 async def menu (callback_query: CallbackQuery):
@@ -24,3 +45,17 @@ async def profile (callback_query: CallbackQuery):
 @user.callback_query(F.data=='order')
 async def order (callback_query: CallbackQuery):
     await callback_query.message.answer('Order not available')
+
+@user.pre_checkout_query() # Регистрирует функцию для обработки события
+async def pre_checkout_query(pre_checkout_query: PreCheckoutQuery, bot: Bot):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+
+@user.message(F.successful_payment)
+async def successful_payment(message: Message):
+    payment_info = message.successful_payment
+    await message.answer(
+        f"Спасибо за оплату, {message.from_user.first_name}! 🎉\n"
+        f"Вы успешно активировали подписку на 1 месяц.\n\n"
+        f"Детали платежа: {payment_info.total_amount / 100} {payment_info.currency}\n"
+        f"Если у вас возникнут вопросы, напишите в поддержку."
+    )
--- tg_app/bot/keyboards/userkb.py
+++ tg_app/bot/keyboards/userkb.py
@@ -4,4 +4,5 @@ main = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Menu', callback_data='menu')],
     [InlineKeyboardButton(text='Order', callback_data='order')],
     [InlineKeyboardButton(text='Profile', callback_data='profile')],
+    [InlineKeyboardButton(text='Donate', callback_data='donate')],
 ])
