b455dd0a7185e774b578be6115276fa81a071d1a
di-m-dy <dimdy11.11@gmail.com>
2025-07-09 12:01:19 +0200

    update conference_requests

--- api_app/datebases/conference_requests.py
+++ api_app/datebases/conference_requests.py
@@ -3,16 +3,16 @@ Module for requesting to MongoDB for conference data
 """
 from datetime import datetime
 from api_app.schemas.conferences import ConferenceModel, StreamModel
+from api_app.schemas.errors import ErrorResponseModel
 from api_app.datebases import config_base as db
 
 from google_services.youtube_api_utils import create_stream_async, create_broadcast_async
 
 # Colletctions
-user_collection = db["users"]
-stream_collection = db["stream"]
-conference_collection = db["conference"]
+stream_collection = db.stream_collection
+conference_collection = db.conference_collection
 
-
+# Requests
 async def get_stream_by_user_id(user_id: int) -> StreamModel:
     """
     Get stream object by user id
@@ -28,6 +28,7 @@ async def get_stream_by_user_id(user_id: int) -> StreamModel:
             )
             await stream_collection.insert_one(StreamModel.model_dump(new_stream, by_alias=True))
             return new_stream
+    return StreamModel.model_validate(stream, by_alias=True)
 
 
 async def insert_conference(
@@ -35,7 +36,7 @@ async def insert_conference(
         listeners: list,
         start_datetime: datetime,
         end_datetime: datetime
-        ) -> ConferenceModel | None:
+        ) -> ConferenceModel | ErrorResponseModel:
     """
     Insert conference object
     :param speaker_id:
@@ -46,28 +47,50 @@ async def insert_conference(
     """
     try:
         stream = await get_stream_by_user_id(speaker_id)
+        if not stream:
+            return ErrorResponseModel(
+                detail="Stream not found for the speaker",
+                status_code=404
+            )
         broadcast = await create_broadcast_async(
             title=f"broadcast for user#{speaker_id}",
             start_datetime=start_datetime,
             end_datetime=start_datetime,
             stream_id=stream.id
         )
+        if not broadcast:
+            return ErrorResponseModel(
+                detail="Failed to create broadcast",
+                status_code=500
+            )
         conference = ConferenceModel(
             speaker_id=speaker_id,
             users=listeners[:],
-            broadcast_id=broadcast.id,
-            recording_url=f"https://youtube.com/live/{broadcast.id}",
+            broadcast_id=broadcast,
+            stream_key=stream.stream_key,
+            recording_url=f"https://youtube.com/live/{broadcast}",
             start_datetime=start_datetime,
             end_datetime=end_datetime
         )
     except Exception as e:
         print(f"Error: {e}")
+        return ErrorResponseModel(
+            detail=f"Error creating conference: {e}",
+            status_code=500
+        )
 
     try:
-        await conference_collection.insert_one(ConferenceModel.model_dump(conference_collection, by_alias=True))
+        await conference_collection.insert_one(ConferenceModel.model_dump(conference, by_alias=True))
         return conference
     except Exception as e:
         if "E11000 duplicate key error" in str(e):
             print("🔁 Duplicate detected")
-            return {"error": "Duplicate key!"}
+            return ErrorResponseModel(
+                detail="Conference already exists",
+                status_code=400
+            )
         print("❌ Unexpected DB error:", type(e), e)
+        return ErrorResponseModel(
+            detail=f"Unexpected error occurred{e}",
+            status_code=500
+        )
