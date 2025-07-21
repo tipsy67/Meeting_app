dc0a3856861b3ae51433948ccdfc8e70eed83536
di-m-dy <dimdy11.11@gmail.com>
2025-07-21 12:13:06 +0200

    refactor: delete temporary route

--- api_app/routers/conference.py
+++ api_app/routers/conference.py
@@ -5,13 +5,11 @@ from starlette import status
 
 from api_app.conference_backend.conference import create_conference
 from api_app.datebases.conference_requests import get_conference
-from api_app.schemas.conferences import ConferenceModel, ConferenceOutputModel, ConferenceCreateModel
+from api_app.schemas.conferences import ConferenceCreateModel
 from api_app.schemas.errors import ErrorResponseModel
 
 from api_app.settings import CONFERENCE_BACKEND
 
-#TODO: temporary import -> delete after rafactoring
-from api_app.datebases.config_base import users_collection
 
 router = APIRouter(prefix="/conferences", tags=["conferences"])
 
@@ -37,16 +35,3 @@ async def conference_detail_rt(conference_id: str):
             status_code=conference.status_code
         )
     return conference.model_dump()
-
-
-#TODO: temporary route, please ask developer of users routers to add this route
-# !!! Need to request detail info about user (for send data to LiveKit App frontend) !!! #
-@router.get("/get_user_detail/{user_id}")
-async def get_user_by_id_rt(user_id: int):
-    """
-    User detail by user_id
-    !!! Temporary Route !!!
-    """
-    user = await users_collection.find_one({"_id": user_id})
-    user["id"] = user.pop("_id")
-    return user
