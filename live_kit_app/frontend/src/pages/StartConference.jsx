d66b6ac72fdde1bf37444235a8a9c3b33bb4d84d
di-m-dy <dimdy11.11@gmail.com>
2025-07-22 00:08:06 +0200

    feat: add ui change for connected/disconnected moderator

--- live_kit_app/frontend/src/pages/StartConference.jsx
+++ live_kit_app/frontend/src/pages/StartConference.jsx
@@ -1,19 +1,51 @@
-import { useParticipants, ParticipantTile, ParticipantName, VideoTrack, useTracks, TrackRefContext, GridLayout, ControlBar, Chat, ChatToggle, LayoutContextProvider, VideoConference } from "@livekit/components-react";
-import { ParticipantEvent, RoomEvent, Track} from "livekit-client";
+import { useRoomContext, VideoConference} from "@livekit/components-react";
+import { RoomEvent } from "livekit-client";
 import { WaitingModerator } from "./WaitingModerator";
+import { useEffect, useState } from "react";
 
 export const StartConference = () => {
   const speakerId = JSON.parse(localStorage.getItem("conference")).speaker.user_id;
   const role = JSON.parse(localStorage.getItem("role"));
-  const participants = useParticipants({updateOnlyOn:RoomEvent.ParticipantConnected});
+  const [speakerJoined, setSpeakerJoined] = useState(false);
+
+  const room = useRoomContext();
+
+  const checkIfSpeakerPresent = () => {
+    return Array.from(room.remoteParticipants).some(
+      (p) => p[0] == String(speakerId)
+    )
+  };
+
   
-  const speakerExist = participants.some(item => item.identity === String(speakerId));
-  if (role === "listener" && !speakerExist) {
-    return <WaitingModerator/>
-  } 
+  useEffect(() => {
+    if (role !== "listener") {
+      setSpeakerJoined(true);
+      return;
+    }
+
+    const updateSpeakerState = () => {
+      const isPresent = checkIfSpeakerPresent();
+      setSpeakerJoined(isPresent);
+    };
+
+    // при маунте сразу проверяем
+    updateSpeakerState();
+
+    // подписываемся на вход/выход
+    room.on(RoomEvent.Connected, () => {updateSpeakerState()});
+    room.on(RoomEvent.ParticipantConnected, (participant) => {participant.identity === String(speakerId) ? setSpeakerJoined(true) : setSpeakerJoined(speakerJoined)});
+    room.on(RoomEvent.ParticipantDisconnected, (participant) => {participant.identity === String(speakerId) ? setSpeakerJoined(false) : setSpeakerJoined(speakerJoined)});
+
+    return () => {
+      room.off(RoomEvent.ParticipantConnected, (participant) => {participant.identity === String(speakerId) ? setSpeakerJoined(true) : setSpeakerJoined(speakerJoined)});
+      room.off(RoomEvent.ParticipantDisconnected, (participant) => {participant.identity === String(speakerId) ? setSpeakerJoined(false) : setSpeakerJoined(speakerJoined)});
+    };
+  }, [room,]);
+
+
   return (
     <> 
-      <VideoConference/>
+      {speakerJoined ? <VideoConference /> : <WaitingModerator />}
     </>
   );
 };
